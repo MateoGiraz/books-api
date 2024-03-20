8fef492106a1798861f94674ace88bbbe220c514
MateoGiraz <mateogiraz27@gmail.com>
2024-03-20 09:28:15 -0300

    create books repository and its corresponding tests

---
+++ helpers/helpers.ts
@@ -0,0 +1,3 @@
+export const indexWasFound = (index: number) => index != -1
+
+export const areEqual = (firstString: string, secondString: string) => firstString === secondString
---
+++ messages.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+  bookNotFound: "Book colud not be found",
+  bookAlreadyExists: "Book already exists",
+  bookIdNotProvided: "Book id not provided"
+}
